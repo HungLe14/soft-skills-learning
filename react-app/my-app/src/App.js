979ac33a512bce0d576cda293dc8bd7846db6aeb
Le Vinh Hung <lv.hung69@gmail.com>
2022-03-05 01:32:23 +0700

    create screen logic + react ini

---
+++ react-app/my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { AddPractice } from "./components/add-practice/AddPractice";
+
+function App() {
+  return (
+    <div className="App">
+      <AddPractice></AddPractice>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/my-app/src/components/add-practice/AddPractice.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./AddPractice.module.css";
+
+export const AddPractice = () => {
+  return (
+    <div>
+      <div className={classes["add-practice"]}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>Thêm đề mục</span>
+      </div>
+    </div>
+  );
+};
