1e1defda9e9bfc37a5be37d287d38d51ebf368f6
Le Vinh Hung <lv.hung69@gmail.com>
2022-03-21 11:30:10 +0700

    :construction_worker:

--- react-app/my-app/src/components/preview/Preview.js
+++ react-app/my-app/src/components/preview/Preview.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Preview.module.css";
 
 export const Preview = () => {
+  const week = useSelector((state) => state.weekArr);
+  const currentWeek = useSelector((state) => state.currentWeek);
+  const currentPractice = useSelector((state) => state.currentPractice);
+  const practice = week[currentWeek - 1].practice[currentPractice - 1];
+  console.log(practice);
+  const currentTest = useSelector((state) => state.currentTest);
+  const test = week[currentWeek - 1].test[currentTest - 1];
+
   const dispatch = useDispatch();
   return (
     <React.Fragment>
@@ -47,8 +55,25 @@ export const Preview = () => {
         </button>
       </div>
       <div className={classes.body}>
-        <img src="" alt="description img" />
-        <p></p>
+        {practice.description.map((des, desIndex) => {
+          console.log(des);
+          return (
+            <React.Fragment key={desIndex}>
+              {des.image.map((image, imageIndex) => {
+                return (
+                  <img
+                    key={imageIndex}
+                    src={`https://firebasestorage.googleapis.com/v0/b/soft-skill-bc141.appspot.com/o/1647492735657-row5-3.png?alt=media`}
+                    alt="description img"
+                    className={classes.previewPicture}
+                  />
+                );
+              })}
+
+              <p className={classes.previewContent}>{des.content}</p>
+            </React.Fragment>
+          );
+        })}
       </div>
     </React.Fragment>
   );
